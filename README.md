63af5b2ba2f07cf4dcf102ad4bc63eb4698c1dc4
byron <byrondalberg@gmail.com>
2022-06-04 22:58:56 -0700

    Most Functions Done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Coding-Quiz
+[LINK TO DEPLOYED APPLICATION(https://youraveragecat.github.io/Coding-Quiz/)]
+# Coding Quiz is a website designed to help students work on their schoolwork, with easy to input flashcards in a simple multidimentional array with a timer.
+### Elements Included
+# Randomized Questions
+# Timer
+# very confusing questions.
+
+### Technologies Used: CSS HTML JavaScript
+
+![Screenshot!](./assets/Screenshot%202022-06-04%20225545.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,41 @@
-var qSet = [
-["Here is the Question", "Wrong", "Right", "Wrong", "Wrong", 3],
-["Heres Another Q", "Answer", "Answer", "Wrong", "Answer"]
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+//>>DONE
+// WHEN I answer a question
+// THEN I am presented with another question
+//>>DONE
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// >>DONE
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+const qSet = [
+["Here is the Question", "Wrong", "Right", "Wrong", "Wrong", "Right"],
+["Heres Another Q", "Answer", "Answer", "Wrong", "Answer", "Wrong"]
 ];//Questons
   // light theme state
-var index
 var startB = document.getElementById("startB")
 var question = document.getElementById("questionLine")
 var answerA = document.getElementById("answerA")
 var answerB = document.getElementById("answerB")
 var answerC = document.getElementById("answerC")
 var answerD = document.getElementById("answerD")
-var timeEl = document.getElementById("timer")
-// Click event causes alert "Hello World"
-// Vanilla JS equivalent: `addEventListener`
-// alertButtonEl.on('click', function () {
-//   alert('Hello World');
-// });
+var timeEl = document.getElementById("timertimer")
+var timeLeft = 30;
+var remainingQ = 4;
+var playerScore = 0;
+
 function startGame(){//Start Game, Timer gets set, Question gets set.
-  document.querySelector(".question").style.display = "block";
-  document.querySelector("#QuizCard").style.display = "none";
-  setTime();
-  displayQ(QIndex());
+  chooseQ();
+  displayQ(QIndex); //Questions Set
 }
-function chooseQ(){
-  QIndex = Math.floor(math.random()* questionsArray.length);
-  console.log(QIndex);
+function chooseQ(){ //RANDOM NUMBER GEN SENDS TO GLOBAL VARIABLE
+  QIndex = Math.floor(Math.random()* qSet.length);
   return QIndex;
 }
 function displayQ(index){//Send Questions And Answers Via Array
@@ -45,33 +56,91 @@ function displayQ(index){//Send Questions And Answers Via Array
   QD = qSet[index][4];
   answerD.textContent = QD; 
   console.log(QD);
-  return(index)
-  //console.log(question+","+answerA+","+answerB+","+answerC+","+answerD);
 }
+//TIMER TIMES THINGS
+    
+var timerId = setInterval(countdown, 1000);
 
-function setTime() { //Timer Function
-  secondsLeft = 30;
-  timerInterval = setInterval(function(){
-    secondsLeft--;
-    timeEl.textContent = "You have "+secondsLeft+" seconds left!"
-    if (secondsLeft === 0){
-      clearInterval(timerInterval);
-      timeUp();
-    }
-  }, 1000);
-}
-displayQ(chooseQ)
-
-
-document.getElementById('answerC').onclick = function(index){
-  correct = qSet[index][5];
-  console.log(correct);
-  if(correct === answerC.textContent){
-    alert("Answer C is Correct")
+function countdown() {
+  if (timeLeft == -1) {
+    clearTimeout(timerId);
+    startGame();
+  } else {
+    timeEl.innerHTML = timeLeft + ' seconds remaining';
+    timeLeft--;
   }
-  else
-  {
-    alert("WRONG, YA SHIT")
+}
+//START GAME
+
+//ONCLICK TO START GAME
+document.getElementById("startB").onclick = function(){
+  startGame();
+  timeLeft = 45;
+}
+//ONCLICK FUNCTIONS FOR EACH ANSWER
+document.getElementById('answerA').onclick = function(index){
+  correctAns = qSet[1][5];
+  console.log(correctAns +"<--RIGHT ANSWER");
+  if(correctAns === answerA.textContent){
+    console.log("goodjob");
+    startGame();
+    playerScore++;
+    timeLeft = 45;
+    remainingQ--;
+    if(remainingQ===0){
+      playerName = prompt("Game Over! Enter Name Here!");
+      window(playerName + " " + playerScore)
+    }
+  }
+  else{
+    console.log("ERRR");
+    timeLeft = timeLeft-10;
+    timeEl.innerHTML = timeLeft + ' seconds remaining';
+  }
+}
+document.getElementById('answerB').onclick = function(index){
+  correctAns = qSet[1][5];
+  console.log(correctAns);
+  if(correctAns === answerB.textContent){
+    console.log("goodjob");
+    startGame();
+    timeLeft = 45;
+    playerScore++;
+  }
+  else{
+    console.log("ERR")
+    timeLeft = timeLeft-10;
+    timeEl.innerHTML = timeLeft + ' seconds remaining';
+  }
+}
+document.getElementById('answerC').onclick = function(index){
+  correctAns = qSet[1][5];
+  console.log(correctAns);
+  if(correctAns === answerC.textContent){
+    console.log("goodjob");
+    startGame();
+    timeLeft = 45;
+    playerScore++;
+  }
+  else{
+    console.log("ERRR")
+    timeLeft = timeLeft-10;
+    timeEl.innerHTML = timeLeft + ' seconds remaining';
+  }
+}
+document.getElementById('answerD').onclick = function(index){
+  correctAns = qSet[1][5];
+  console.log(correctAns);
+  if(correctAns === answerD.textContent){
+    console.log("goodjob");
+    startGame();
+    timeLeft = 45;
+    playerScore++;
+  }
+  else{
+    console.log("ERRRR");
+    timeLeft = timeLeft-10;
+    timeEl.innerHTML = timeLeft + ' seconds remaining';
   }
 }
 //setTime()
